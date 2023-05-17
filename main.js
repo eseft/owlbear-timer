cc019e18f94d4a33821fb43f188c8295f0af4fee
Dmitry Ivanov <ivanofdm@gmail.com>
2023-05-17 20:43:54 +0300

    Improvments:
    - Added sound effect to signalize timer end;
    - GM and Player windows separated.

--- main.js
+++ main.js
@@ -6,38 +6,57 @@ import OBR from "@owlbear-rodeo/sdk";
 
 const urlRoutes = {
   "/": {
-    template: "templates/main.html",
-    script: function () {
+    template: {
+      "GM": "templates/gm.html",
+      "PLAYER": "templates/player.html",
+    },
+    script: function (role) {
+      if (role == "PLAYER") { return; }
       setupTimer();
     }
 
   },
   "/timer": {
-    template: "templates/timer.html",
-    script: function () {
+    template: {
+      "GM": "templates/timer.html",
+      "PLAYER": "templates/timer.html",
+    },
+    script: function (role) {
       startTimer();
     }
   }
 };
 
 const urlLocationHandler = async () => {
-  const location = window.location.pathname;
+  // OBR role (GM or PLAYER)
+  const role = await OBR.player.getRole();
+  // Current location
+  let location = window.location.pathname;
+  // Set location to "/" if it's not set
   if (location.length == 0) {
     location = "/";
   }
 
+  // If there is no given location in urlRoutes set it to "/"
   const route = urlRoutes[location] || urlRoutes["/"];
 
-  const html = await fetch(route.template).then((response) =>
+  // Get needed html template
+  const html = await fetch(route.template[`${role}`]).then((response) =>
     response.text());
 
+  // Set html template
   document.getElementById("app").innerHTML = html;
-  route.script();
+  // Execute script for a current location
+  route.script(role);
 };
 
-urlLocationHandler();
 
 OBR.onReady(async () => {
+  // Routes happen only if Owlbear Ready
+  // Otherwise we didn't get Role
+  urlLocationHandler();
+
+  // Function will be executed any time room metadata changes
   OBR.room.onMetadataChange(async (metadata) => {
     manageTimer(metadata);
   })
